5f930f3e693658daf364210a4a6de97838784b9a
Uzair <uzairarifjokhio@gmail.com>
2023-11-07 19:43:23 +0500

    Java Start

---
+++ Student.java
@@ -0,0 +1,13 @@
+public class Student {
+    // fields of data
+    String Dept;
+    String  Name;
+    double CGPA;
+    private double marks = 86; 
+
+    void result(){
+        System.err.println("Name: " + Name +  "\nCGPA: " + CGPA + "\nDepartment: " + Dept );
+    }
+
+
+}
