7f7b9d23692659529fb7013b2c8fea75c0b7cf66
Uzair <uzairarifjokhio@gmail.com>
2023-11-08 00:24:03 +0500

    just some basis of java

---
+++ Math.java
@@ -0,0 +1,34 @@
+public class Math {
+
+
+    void Powtable(){
+        int a = 1 ;
+        System.out.println("a\t"+ "a^2\t" +  "a^3\t");
+        for (int i= 1; i <= 4;i++){
+            System.out.println(a + "\t" +Math.pow(a, 2)+ "\t" + Math.pow(a,3));
+            a += 1;
+        }
+    }
+    
+
+
+    void sum(){
+        double a = 9.5, b=4.5, c= 2.5, d = 3, e =45.5, f =3.5;
+        double x = (a*b) - (c*d);
+        double y = e - f;
+        double z = x/y;
+        System.out.println("the answer is: " + z);  
+    }
+
+    void series(){
+        int a = 1;
+        int temp = 0;
+        int num = 0;
+        for (int i =0 ;i<=9;i++){
+            temp += a;
+            num = temp + a ;
+            System.out.println(num);
+        }
+        
+    }
+}
--- Uni.java
+++ Uni.java
@@ -1,10 +1,14 @@
 public class Uni {
-    public static void main(String[] args){
-        Student s1 = new Student();
-        s1.Name = "Shamir";
-        s1.Dept = "Industrial Engineering";
-        s1.CGPA = 3.65;
-        s1.result();
+    // public static void main(String[] args){
+    //     Student s1 = new Student();
+    //     s1.Name = "Shamir";
+    //     s1.Dept = "Industrial Engineering";
+    //     s1.CGPA = 3.65;
+    //     s1.result();
 
+    // }
+    public static void main(String[] args){
+        Math shot = new Math();
+        shot.series();
     }
 }
