a70ba370540118e73ffc5f651d85991c897496a8
Uzair <uzairarifjokhio@gmail.com>
2023-12-10 17:57:53 +0500

    ...

--- MathTester.java
+++ MathTester.java
@@ -1,12 +1,5 @@
 public class MathTester {
-    // public static void main(String[] args){
-    //     Student s1 = new Student();
-    //     s1.Name = "Shamir";
-    //     s1.Dept = "Industrial Engineering";
-    //     s1.CGPA = 3.65;
-    //     s1.result();
 
-    // }
     public static void main(String[] args){
         Mathjava shot = new Mathjava();
         shot.series();
